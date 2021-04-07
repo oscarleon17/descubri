c271d9af581fa2cfe39d84bd284cd02f524eec7d
Oscar Leon <oscarleon@oscars-mbp.lan>
2021-04-07 01:07:21 -0400

    Changing onboarding screen to pageview and routing to home. Misc iOS fixes.

--- lib/screens/DescubriOnboarding.dart
+++ lib/screens/DescubriOnboarding.dart
@@ -2,39 +2,39 @@ import 'package:descubri/screens/DescubriHome.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-
-class DescubriOnboarding extends StatefulWidget{
+class DescubriOnboarding extends StatefulWidget {
   @override
   _DescubriOnboardingState createState() => _DescubriOnboardingState();
 }
 
 class _DescubriOnboardingState extends State<DescubriOnboarding> {
-  final int _numPages = 3;
-  final PageController _pageController = PageController(initialPage: 0);
-  int _currentPage = 0;
-
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Press the button below to go home'),
-          ],
+    final PageController controller = PageController(initialPage: 0);
+
+    return PageView(
+      scrollDirection: Axis.horizontal,
+      controller: controller,
+      children: <Widget>[
+        Center(
+          child: Text('First Page'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => DescubriHome()),
-          );
-        },
-        tooltip: "Go Home",
-        child: Icon(Icons.home),
-      ),
+        Center(
+          child: Text('Second Page'),
+        ),
+        Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DescubriHome()),
+              );
+            },
+            tooltip: "Go Home",
+            child: Icon(Icons.home),
+          ),
+        )
+      ],
     );
   }
 }
