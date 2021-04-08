35bdc39258b1b432b96b9a922b318957f297d447
oscarleon17 <oaleon17@outlook.com>
2021-04-07 23:21:01 -0400

    Changed added scaffolds to each pageview and setup text alignment

--- lib/screens/DescubriOnboarding.dart
+++ lib/screens/DescubriOnboarding.dart
@@ -7,6 +7,7 @@ class DescubriOnboarding extends StatefulWidget {
   _DescubriOnboardingState createState() => _DescubriOnboardingState();
 }
 
+//TODO: Add app description text for each page along with visuals, change form of home button.
 class _DescubriOnboardingState extends State<DescubriOnboarding> {
   @override
   Widget build(BuildContext context) {
@@ -16,23 +17,55 @@ class _DescubriOnboardingState extends State<DescubriOnboarding> {
       scrollDirection: Axis.horizontal,
       controller: controller,
       children: <Widget>[
-        Center(
-          child: Text('First Page'),
-        ),
-        Center(
-          child: Text('Second Page'),
-        ),
         Scaffold(
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => DescubriHome()),
-              );
-            },
-            tooltip: "Go Home",
-            child: Icon(Icons.home),
+            body: Container(
+          child: Center(
+              child: new Column(
+                  children: [Text('First Page'), Text('Subtext')],
+                  mainAxisAlignment: MainAxisAlignment
+                      .center, //Center Column contents horizontally,
+                  crossAxisAlignment: CrossAxisAlignment
+                      .center //Center Column contents vertically,
+                  )),
+        )),
+        Scaffold(
+            body: Container(
+          child: Center(
+              child: new Column(
+                  children: [Text('Second Page')],
+                  mainAxisAlignment: MainAxisAlignment
+                      .center, //Center Column contents horizontally,
+                  crossAxisAlignment: CrossAxisAlignment
+                      .center //Center Column contents vertically,
+                  )),
+        )),
+        Scaffold(
+          body: Container(
+            child: Center(
+                child: new Column(
+                    children: [Text('Third Page')],
+                    mainAxisAlignment: MainAxisAlignment
+                        .center, //Center Column contents horizontally,
+                    crossAxisAlignment: CrossAxisAlignment
+                        .center //Center Column contents vertically,
+                    )),
           ),
+          floatingActionButton: Container(
+              child: SizedBox(
+                  height: 60,
+                  width: 60,
+                  child: FloatingActionButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => DescubriHome()),
+                      );
+                    },
+                    tooltip: "Go Home",
+                    child: Icon(Icons.home),
+                  ))),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerFloat,
         )
       ],
     );
